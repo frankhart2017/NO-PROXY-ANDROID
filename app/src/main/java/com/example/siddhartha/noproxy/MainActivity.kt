1ca4373a05cf5d59f8fa91fc71cbb5634938f3d3
frankhart2017 <sdharchou@gmail.com>
2018-04-26 17:37:59 +0530

    Add forgot activity

--- app/src/main/java/com/example/siddhartha/noproxy/MainActivity.kt
+++ app/src/main/java/com/example/siddhartha/noproxy/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.siddhartha.noproxy
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun forgotBtnClicked(view: View) {
+        val gotoForgot = Intent(this, ForgotActivity::class.java)
+        startActivity(gotoForgot)
     }
 }
